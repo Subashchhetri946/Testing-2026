94f6831e6a248b081f70cfd81acd06616236526d
Subash Chhetri <156183094+Subashchhetri946@users.noreply.github.com>
2026-09-09 21:23:08 +0300

    added triangle.py

---
+++ triangle/triangle.py
@@ -0,0 +1,15 @@
+def check_triangle(a, b, c):
+
+    if a == b and b == c:
+        return "Equilateral triangle"
+
+    elif a == b or a == c or b == c:
+        return "Isosceles triangle"
+
+    else:
+        return "Irregular triangle"
+
+
+    a = int(input("Enter the first side of the triangle:"))
+    b = int(input("Enter the second side of the triangle:"))
+    c = int(input("Enter the third side of the triangle:"))
